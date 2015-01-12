6d7281f20a41a44e09c5a61ff01e2b9be3ce8c3e
Steven Jack <stevenmajack@gmail.com>
2015-01-12 06:39:29 +0000

    Remove old code block and add "the_content" filter

--- wp-content/themes/blain/tour-list-page.php
+++ wp-content/themes/blain/tour-list-page.php
@@ -21,14 +21,14 @@ get_header(); ?>
                 <div class="col-md-4">
                     <h1><?= $post->post_title ?></h1>
                     <?= types_render_field('tour-list-image', array("output" => "image")) ?></div>
-                <div class="col-md-8"></p><?= $post->post_content ?></p></div>
+                <div class="col-md-8"></p><?= apply_filters('the_content', $post->post_content) ?></p></div>
 			</div>
 
             <?php foreach ($children as $child_id => $child) { ?>
                 <h3><?php echo $child->post_title; ?></h3>
                 <?php $tour_children = get_children(array('post_parent' => $child_id)); ?>
                 <?php if (!empty($child->post_content)) { ?>
-                    <p><?= $child->post_content; ?></p>
+                    <p><?= apply_filters('the_content', $child->post_content); ?></p>
                 <?php } ?>
                 <ul>
                 <?php foreach ($tour_children as $tour_child_id => $tour_child) { ?>
@@ -36,29 +36,6 @@ get_header(); ?>
                 <?php } ?>
                 </ul>
             <?php } ?>
-
-
-			<?php $tours = get_group('classic_tours');
-				$index = 0;
-			      foreach($tours as $tour) {
-			?>
-				<div class="row">
-  					<div class="col-md-6"><img src="<?= $tour['classic_tours_image'][1]['original'] ?>"></div>
-  					<div class="col-md-6">
-						<div class="title">
-							<h4><?= $tour['classic_tours_title'][1] ?></h4>
-							<span><?= $tour['classic_tours_duration'][1] ?></span>
-							</span>&pound;<?= $tour['classic_tours_price'][1] ?></span>
-						</div>
-						<div class="blurb">
-							<?= get('classic_tours_blurb') ?>
-						</div>
-					</div>
-				</div>
-			<?php
-			$index++;
-			 }
-			?>
 
 			<?php endwhile; // end of the loop. ?>
 		</main><!-- #main -->
