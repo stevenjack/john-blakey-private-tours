6f45cdb805b7aae9930197f8a21eb158285b20ae
Steven Jack <stevenmajack@gmail.com>
2015-03-06 19:22:52 +0000

    Added new layout type

---
+++ wp-content/themes/blain/custom-content-page-float.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template Name: Content page (image wrap)
+ *
+ * @package Blain
+ */
+
+get_header(); ?>
+
+	<div id="tour" class="content-area">
+        <main id="main" class="site-main tour-main content" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+                <?php $post = get_post(); ?>
+                <div class="entry-header">
+                    <h1 class="entry-title"><?= $post->post_title ?></h1>
+                </div>
+				<div class="row">
+  					<div class="col-md-12 tour-image content">
+                        <?= types_render_field('default-content-image', array("output" => "image")) ?>
+						<div class="blurb"><?= apply_filters('the_content', $post->post_content) ?></div>
+					</div>
+				</div>
+			<?php endwhile; // end of the loop. ?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
