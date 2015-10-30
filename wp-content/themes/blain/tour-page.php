c5b1bf128e192af01cd0f3783b9ad387f5ead134
Steven Jack <stevenmajack@gmail.com>
2015-10-30 08:13:50 +0000

    Removes pricing

--- wp-content/themes/blain/tour-page.php
+++ wp-content/themes/blain/tour-page.php
@@ -7,31 +7,29 @@
 
 get_header(); ?>
 
-	<div id="tour" class="content-area">
+    <div id="tour" class="content-area">
         <main id="main" class="site-main tour-main" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
+            <?php while ( have_posts() ) : the_post(); ?>
                 <?php $post = get_post(); ?>
 
-				<div class="row">
+                <div class="row">
                     <div class="col-md-6"><?= types_render_field('tour-image', array("output" => "image")) ?></div>
-  					<div class="col-md-6">
-						<div class="title">
+                    <div class="col-md-6">
+                        <div class="title">
                             <div class="row">
                                 <div class="col-md-9">
                                     <h4 class="inner-title"><?= $post->post_title ?></h4>
                                 </div>
-                                <div class="col-md-3">
-                                    <h4 class="price">&pound;<?= types_render_field('tour-price', array('output' => 'raw'))  ?></h4>
-                                </div>
-						</div>
-<div style="clear: both;"></div>
-						<div class="blurb">
-							<?= apply_filters('the_content', $post->post_content) ?>
-						</div>
-					</div>
-				</div>
-			<?php endwhile; // end of the loop. ?>
-		</main><!-- #main -->
-	</div><!-- #primary -->
+                            </div>
+                        </div>
+                        <div style="clear: both;"></div>
+                        <div class="blurb">
+                            <?= apply_filters('the_content', $post->post_content) ?>
+                        </div>
+                    </div>
+                </div>
+            <?php endwhile; // end of the loop. ?>
+        </main><!-- #main -->
+    </div><!-- #primary -->
 
 <?php get_footer(); ?>
